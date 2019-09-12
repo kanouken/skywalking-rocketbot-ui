9fbfe240662175b9be11ff2340051824a54c9829
kanouken <1462927100@qq.com>
2019-09-12 13:39:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 <template>
-  <div id="app">
-    <RkHeader :time="stateGlobal.duration"/>
-    <router-view class="wrapper"/>
-  </div>
+  <router-view></router-view>
 </template>
-
 <script lang="ts">
-import Vue from 'vue';
-import { State } from 'vuex-class';
-import { Component } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
+import getUrlKey from "./utils/urlGet";
 
-@Component({
-})
+@Component
 export default class App extends Vue {
-  value = [];
-  local = {
-    hourTip: 'Select Hour', // tip of select hour
-    minuteTip: 'Select Minute', // tip of select minute
-    secondTip: 'Select Second', // tip of select second
-  };
-  @State('global') stateGlobal;
+    private created(){
+        let projectKey = getUrlKey('projectKey')
+        localStorage.setItem("projectKey",projectKey)
+    }
 }
 </script>
-
-<style lang='scss'>
+<style>
+@import './assets/styles/reset.scss';
+@import './assets/styles/lib.scss';
+@import './assets/styles/style.scss';
+@import './assets/styles/grid.scss';
+#app,.container{
+  height: 100%;
+}
+#app{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 </style>
